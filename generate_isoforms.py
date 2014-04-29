2e13df46bb74d231a5c88a20f7d0dc20980ab8d1
Alex Amlie-Wolf <alexamlie@gmail.com>
2014-04-28 20:01:58 -0400

    Added two scripts that deal with generating gene isoforms and visualizing the coverage calculations over those isoforms

---
+++ generate_isoforms.py
@@ -0,0 +1,63 @@
+"""
+generate_isoforms.py
+Alex Amlie-Wolf, Spring 2014
+
+A program that takes in a refseq formatted file containing exon starts and ends for different
+transcripts and outputs a bed file containing windows for all of the exons and introns of that
+gene. This can't be done (as far as I know) directly from the UCSC table downloads or bedtools,
+as they restrict you to making a bed interval for either exons or introns, or the whole gene, etc
+
+Only works with Python 2.7 and above
+"""
+
+import argparse
+
+def ref_to_bed(ref_file, out_file):
+    # define a convenienec dict to access the refseq file
+    ref_coords = {'bin': 0, 'cdsEnd': 7, 'cdsEndStat': 14, 'cdsStart': 6, 'cdsStartStat': 13,
+ 'chrom': 2, 'exonCount': 8, 'exonEnds': 10, 'exonFrames': 15, 'exonStarts': 9, 'name': 1,
+ 'name2': 12, 'score': 11, 'strand': 3, 'txEnd': 5, 'txStart': 4}
+
+    with open(ref_file, 'r') as refseq, open(out_file, 'w') as outfile:
+        for gene in refseq:
+            genedata = gene.strip().split("\t")
+
+            # to go through the windows of the gene (introns and exons), we need to make them
+            # into lists. skips the last character because it's a comma
+            exonstarts = genedata[ref_coords['exonStarts']][:-1].split(',')
+            exonends = genedata[ref_coords['exonEnds']][:-1].split(',')
+
+            # build up the lists of introns
+            intronstarts = []
+            intronends = []
+            # first see if the gene starts with an intron (may not ever happen)
+            if int(exonstarts[0]) > int(genedata[ref_coords["txStart"]]):
+                intronstarts.append(genedata[ref_coords["txStart"]])
+                intronends.append(exonstarts[0])
+            # loop through exons (this assumes the lists are the same length)
+            for i in range(len(exonstarts)-1):
+                intronstarts.append(exonends[i]) # intron starts where exon ends
+                intronends.append(exonstarts[i+1]) # intron ends where exon starts
+            # now check if there is a 3' intron/UTR/whatever
+            if int(exonends[len(exonends)-1]) < int(genedata[ref_coords["txEnd"]]):
+                intronstarts.append(exonends[len(exonends)-1])
+                intronends.append(genedata[ref_coords['txEnd']])
+
+            # now just make the bed lines
+            # bed format: chr start end name score strand
+            ## write the exons
+            for i in range(len(exonstarts)):
+                outfile.write("\t".join([genedata[ref_coords['chrom']], exonstarts[i], exonends[i], genedata[ref_coords["name"]]+"_exon_"+str(i), "0", genedata[ref_coords["strand"]]])+"\n")
+
+            ## write the introns
+            for i in range(len(intronstarts)):
+                outfile.write("\t".join([genedata[ref_coords['chrom']], intronstarts[i], intronends[i], genedata[ref_coords["name"]]+"_intron_"+str(i), "0", genedata[ref_coords["strand"]]])+"\n")
+
+        
+if __name__=="__main__":
+    # create argparser
+    parser = argparse.ArgumentParser(description="Turn a refseq file into a .bed file with exon and intron windows")
+    parser.add_argument("refseq_file", help="The refseq file. Should be in refseq format")
+    parser.add_argument("output", help="The output file you wish to use")
+    pargs = parser.parse_args()
+    ref_to_bed(pargs.refseq_file, pargs.output)
---
+++ multicov_to_bedgraph.py
@@ -0,0 +1,55 @@
+'''
+multicov_to_bedgraph.py
+Alex Amlie-Wolf
+A program to convert the output from a bedtools multicov file into a series of
+bedGraph files, for visualization on the genome browser
+'''
+
+import sys
+
+def convert_multicov(cov_file, outdir):
+    cov = open(cov_file, 'r')
+    header = cov.readline().strip().split("\t")
+    ## the first 5 entries are chr, start, end, name, score, and strand
+    exps = header[6:]
+    ## make a dict of file descriptors
+    exp_files = {}
+    for i in exps:
+        exp_files[i] = open(outdir+i+"_coverage.bedgraph", 'w')
+
+    ## hard code different colors in the NLS experiment
+    exp_colors = {"bg": "000,000,200", "nt":"200,000,000"}
+        
+    ## write the first line of the bedgraph format
+    for exp in exp_files:
+        if exp[:2] in exp_colors.keys():
+            formatline = " ".join(["track type=bedGraph", "name="+exp, "visibility=full", "color="+exp_colors[exp[:2]]])
+        else:
+            formatline = " ".join(["track type=bedGraph", "name="+exp, "visibility=full", "color=000,000,200"])
+        exp_files[exp].write(formatline+"\n")
+
+    ## we want to keep track of which exons/introns we've seen already
+    ## this will need to be optimized if i use this on a bigger file
+    start_list = []
+    end_list = []    
+        
+    ## now split the actual entries
+    for entry in cov:
+        data = entry.strip().split("\t")
+        ## only write the data if we haven't seen it already
+        if data[1] not in start_list and data[2] not in end_list:
+            start_list.append(data[1])
+            end_list.append(data[2])
+            chrdata = "\t".join(data[:3])
+            for i in range(len(exps)):
+                ## write the chromosome data and then the counts (in the right order)            
+                exp_files[exps[i]].write("\t".join([chrdata, data[i+6]])+"\n")
+        
+    [exp_files[i].close() for i in exp_files]
+    cov.close()
+
+if __name__=="__main__":
+    if len(sys.argv) != 3:
+        print "Usage: "+sys.argv[0]+" <multicov file> <output directory>"
+    else:
+        convert_multicov(sys.argv[1], sys.argv[2])
